175282617f18b73d2e7674124fd871b4e5dc1746
AbdelhakimTelmoudy <telmoudy.abdelhakim@gmail.com>
2024-01-06 22:03:53 +0100

    fetch: add images

--- EbankClient/src/main/java/org/example/Main.java
+++ EbankClient/src/main/java/org/example/Main.java
@@ -6,14 +6,10 @@ import org.example.proxy.EbankServiceService;
 
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         EbankService stup = new EbankServiceService().getEbankServicePort();
-
         List<Account> list =stup.getAccounts();
-
         for (int i=0;i<list.size();i++){
             System.out.println("Fullname =>"+list.get(i).getFullname());
         }
--- TP2_JAXWS/src/main/java/org/example/web/EbankService.java
+++ TP2_JAXWS/src/main/java/org/example/web/EbankService.java
@@ -4,26 +4,19 @@ import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
 import org.example.entities.Account;
-
 import java.util.List;
-
 @WebService(name = "ebankService")
 public class EbankService {
-
     @WebMethod()
     public double convertion(double montant){
-
         return montant*23;
     }
-
     @WebMethod()
     public Account getAccount(@WebParam(name = "id") int id){
-
         return new Account(id,"hakim","hakim@gmail.com");
     }
     @WebMethod()
     public List<Account> getAccounts(){
-
         return List.of(
                 new Account(1,"hakim","hakim@gmail.com"),
                 new Account(2,"hasan","hasan@gmail.com"),
